ca7af6d4f72e2ae4ddc75c9a411d2d147b8c6253
ElTrompa <anroiz@alumnatflorida.es>
2025-02-03 15:55:02 +0100

    final

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"proba","c":"main","l":"main()","u":"%3Cinit%3E()"},{"p":"proba","c":"main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- main.java
+++ main.java
@@ -2,14 +2,25 @@ package proba;
 
 import java.util.Scanner;
 
+/**
+ * Clase principal que solicita el nombre del usuario e imprime un saludo.
+ */
 public class main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(Hola, cual es tu nombre);
-		String nombre = scanner.next();
-		
-		System.out.println(Hola,  + nombre);
-	}
-	
+
+    /**
+     * Método principal que ejecuta la aplicación.
+     * Solicita al usuario su nombre a través de la consola y luego muestra un saludo personalizado.
+     *
+     * @param args Argumentos de línea de comandos (no utilizados en este programa).
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Objeto Scanner para leer la entrada del usuario
+        System.out.println(Hola, cual es tu nombre); // Mensaje de solicitud
+
+        String nombre = scanner.next(); // Captura el nombre ingresado por el usuario
+
+        System.out.println(Hola,  + nombre); // Saludo personalizado
+        scanner.close(); // Cierra el Scanner para liberar recursos
+    }
 }
 
