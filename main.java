986b723b3f952adb65ecf06f20561c373537d856
ElTrompa <anroiz@alumnatflorida.es>
2025-02-03 15:38:54 +0100

    Añadido saludo personalizado

--- main.java
+++ main.java
@@ -3,24 +3,13 @@ package proba;
 import java.util.Scanner;
 
 public class main {
-	
 	public static void main(String[] args) {
-		float PrecioFabrica = 1;
-		float PrecioReal = 1;
-		float PorcentajePrecio = 1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(Hola, cual es tu nombre);
+		String nombre = scanner.next();
 		
-		Scanner teclado = new Scanner(System.in);
-		
-		System.out.println("que costa de fabrica? ");
-		PrecioFabrica = teclado.nextInt();
-		System.out.println("Seguro? "+PrecioFabrica);
-		
-		System.out.println("que costa de real? ");
-		PrecioReal = teclado.nextInt();
-		System.out.println("Seguro? "+PrecioReal);
-		
-		PorcentajePrecio = PrecioFabrica/PrecioReal*100;
-		System.out.print("El porcentaje es: "+PorcentajePrecio+"%");
+		System.out.println(Hola,  + nombre);
 	}
 	
 }
+
